e614abf1caa7ef61f017743cf3633a04223ef8dd
Jolville <jesse.colville@outlook.com>
2024-03-09 13:00:25 +1100

    image name

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ echo "Current tag: $current_tag"
 echo "New tag?"
 read new_tag
 
-docker build -t australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum:$new_tag .
+docker build -t australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag .
 
-docker push australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum:$new_tag
+docker push australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag
 
 git tag $new_tag
 
