f5b75473d5bc54dd0d4fc89aab8ee6a4a9ff59cf
Jolville <jesse.colville@outlook.com>
2024-03-09 12:56:01 +1100

    wip deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+git diff --exit-code
+
+current_tag=$(git tag)
+echo "Current tag: $current_tag"
+echo "New tag?"
+read new_tag
+
+docker build -t australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum:$new_tag .
+
+docker push australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum:$new_tag
+
+git tag $new_tag
+
+git push origin $new_tag
