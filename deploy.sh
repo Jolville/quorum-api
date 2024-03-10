dc9a122d2f9a35b11cff632572dc7555b61c32c0
Jolville <jesse.colville@outlook.com>
2024-03-10 16:22:08 +1100

    fix service name

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,8 @@ docker build -t australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum
 
 docker push australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag
 
-gcloud run deploy SERVICE --image australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag \
+gcloud run deploy three-tier-app-api \
+    --image australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag \
     --update-secrets=JWT_SECRET=JWT_SECRET:1,DATABASE_URL=DATABASE_URL:1
 
 git tag $new_tag
