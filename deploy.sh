70971a49f23ad204d43d325e7f1d1eee4fcc2424
Jolville <jesse.colville@outlook.com>
2024-03-31 10:34:01 +1100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,11 @@ echo "Current tag: $current_tag"
 echo 'New tag (set to "latest" to re-deploy latest image)?'
 read new_tag
 
-if [$new_tag != "latest"]; then
+if [ $new_tag != "latest" ]; then
     docker build --platform linux/amd64 -t australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag .
     docker push australia-southeast1-docker.pkg.dev/trusty-charmer-415303/quorum/quorum-api:$new_tag
+else
+    new_tag=$current_tag
 fi
 
 gcloud run deploy three-tier-app-api \
@@ -21,6 +23,7 @@ gcloud run deploy three-tier-app-api \
     --set-env-vars $env_vars_string \
     --region australia-southeast1
 
-git tag $new_tag
-
-git push origin $new_tag
+if [ $new_tag != $current_tag ]; then
+    git tag $new_tag
+    git push origin $new_tag
+fi
